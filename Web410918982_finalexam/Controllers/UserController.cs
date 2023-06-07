468a623e3735e5f59c333f347b85e4abd960a2ec
盧子若 <s1091898@gm.edu.tw>
2023-06-07 20:03:50 +0800

    0607

---
+++ Web410918982_finalexam/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web410918982_finalexam.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Music()
+        {
+            
+            
+            return View();
+        }
+    }
+}
